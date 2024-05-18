c1351c595b49b369724ef4084ff6f4f9ef380c1e
Fatima Ramone <fatimapitaemail@gmail.com>
2024-05-18 18:19:55 +0200

    Subida - 20240518181954

--- upload_git_FUNCIONA.ps1
+++ upload_git_FUNCIONA.ps1
@@ -7,22 +7,32 @@ Set-Location -Path "C:\Users\Admin\Desktop\daily"
 # Obtener la fecha y hora actual
 $timestamp = Get-Date -Format "yyyyMMddHHmmss"
 
-# Renombrar el archivo upload.html con la fecha y hora actual
-Rename-Item -Path "upload.html" -NewName "upload_$timestamp.html"
+# Buscar el primer archivo con extensión .html en el directorio
+$html_file = Get-ChildItem -Filter *.html | Select-Object -First 1
 
-# Inicializar un repositorio Git si no está inicializado
-if (!(Test-Path -Path ".git")) {
-    git init
+# Verificar si se encontró algún archivo HTML
+if ($html_file -ne $null) {
+    # Renombrar el archivo HTML encontrado con la fecha y hora actual
+    Rename-Item -Path $html_file.FullName -NewName "upload_$timestamp.html"
+
+    # Inicializar un repositorio Git si no está inicializado
+    if (!(Test-Path -Path ".git")) {
+        git init
+    }
+
+    # Agregar todos los archivos al repositorio
+    git add .
+
+    # Confirmar los cambios con un mensaje que incluya la fecha y hora
+    git commit -m "Subida - $timestamp"
+
+    # Subir los cambios al repositorio remoto en la rama master
+    git push origin master
+
+    Write-Host "La subida se realizó correctamente."
+} else {
+    Write-Host "No se encontraron archivos HTML en el directorio actual."
 }
-
-# Agregar todos los archivos al repositorio
-git add .
-
-# Confirmar los cambios con un mensaje que incluya la fecha y hora
-git commit -m "Subida - $timestamp"
-
-# Subir los cambios al repositorio remoto en la rama master
-git push origin master
 
 # Volver al directorio original
 Set-Location -Path $directorio_actual
